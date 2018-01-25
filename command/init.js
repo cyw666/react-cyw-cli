73e8449a18e70bb2436b0ab82ab009afd44e6a60
cyw666 <13656631764@163.com>
2018-01-25 13:11:26 +0800

    init update

--- command/init.js
+++ command/init.js
@@ -6,30 +6,43 @@ const config = require('../templates')
 const chalk = require('chalk')
 
 module.exports = () => {
-  co(function* () {
-    let gitUrl
-    let branch
+  co(function *() {
     // 处理用户输入
     let tplName = yield prompt('Template name: ')
+    if(!tplName){
+      tplName = "react-start"
+    }
     if (!config.tpl[tplName]) {
       console.log(chalk.red('\n × Template does not exit!'))
       process.exit()
     }
-    let projectName = yield prompt('Project name: ');
+    let projectName = yield prompt('Project name: ')
+    if(!projectName){
+      projectName = tplName;
+    }
+    let gitUrl
+    let branch
+    
+    if (!config.tpl[tplName]) {
+      console.log(chalk.red('\n × Template does not exit!'))
+      process.exit()
+    }
     gitUrl = config.tpl[tplName].url
     branch = config.tpl[tplName].branch
+    
     // git命令，远程拉取项目并自定义项目名
     let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName}`
     
     console.log(chalk.white('\n Start generating...'))
-    exec(cmdStr, (error, stdout, stderr) = > {
-      if(error) {
+    
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (error) {
         console.log(error)
         process.exit()
       }
       console.log(chalk.green('\n √ Generation completed!'))
-    console.log(`\n cd ${projectName} && npm install \n`)
-    process.exit()
-  })
+      console.log(`\n cd ${projectName} && npm install \n`)
+      process.exit()
+    })
   })
 }
